9588e2c9b53039dc5c0183645783f2b0eb646a4d
Thiago Benkendorf <mail@tr3ntu.com>
2021-12-15 03:11:12 +0100

    Commit main - Fibonacci

---
+++ .idea/me/tr3ntu/fibonacci/main.java
@@ -0,0 +1,16 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        fibonacci(6);
+        System.out.println(fibonacci(n));
+
+    }
+
+    public static long fibonacci(long n) {
+        fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2);
+
+    }
+
+}
+
